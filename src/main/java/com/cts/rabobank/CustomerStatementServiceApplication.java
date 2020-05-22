28c01f34b008e173dbaad438732e303adecfdfea
venkateshkumard <venkateshkumar_d@yahoo.com>
2020-05-22 15:22:04 +0530

    Update CustomerStatementServiceApplication.java

--- src/main/java/com/cts/rabobank/CustomerStatementServiceApplication.java
+++ src/main/java/com/cts/rabobank/CustomerStatementServiceApplication.java
@@ -31,6 +31,6 @@ public class CustomerStatementServiceApplication {
     
     @GetMapping(value = "/")
     public String index() {
-        return "Welcome Venkat, Sample application demo deployed via CI-CD Azure pipeline";
+        return "Welcome Venkat, Rabo application demo deployed via CI-CD Azure pipeline";
     }
 }
